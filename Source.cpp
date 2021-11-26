4d94ae618dc334e41a48818b761c58c93515cc43
Сергій Пазюк <89936215+SerhiyPaziyc@users.noreply.github.com>
2021-11-26 10:53:43 +0200

    laba

--- Source.cpp
+++ Source.cpp
@@ -1,47 +1,36 @@
-#include <iostream>  
+#include <iostream>
+#include <iomanip>
 using namespace std;
-
-void diapazon(int* a,int i, const int n) 
+void Print(int* a, const int size, int i)
 {
-	if (i < n) {
-		a[i] = 1 + rand() % (40);
-		cout << a[i] << " ";
-		diapazon(a, i + 1, n);
-	}
+    a[i] = i;
+    cout << setw(4) << a[i];
+    if (i < size - 1)
+        Print(a, size, i + 1);
+    else
+        cout << endl;
+}
+int Sum(int* a, const int size, int i)
+{
+    if (i < size)
+    {
+        if (a[i] % 2 == 1)
+            return i + Sum(a, size, i + 1);
+        else
+            return Sum(a, size, i + 1);
+    }
+    else
+        return 0;
 }
 
-void vuznachutu(int* a, int i, int n, int& nepar) 
+int main()
 {
-	if (i < n) {
-		if (a[i]%2 !=0 )
-		{
-			nepar = a[i];
-		
-		}
-		
-		vuznachutu(a, i + 1, n, nepar );
-	}
-
-}
-int sum(int indexnepar) 
-{
-	int S = indexnepar * 1.;
-	return S;
-}
-
-int main() {
-	srand((unsigned)time(NULL));
-	const int n = 10;
-	int a[n + 1];
-	int i{};
-	int nepar;
-
-	diapazon(a, i, n + 1);
-	vuznachutu(a, i, n, nepar);
-
-	cout << endl;
-	cout << "nepar: " << nepar  << endl;
-	
-	cout << "suma: " << sum(nepar) << endl;
-	return 0;
+    int n;
+    cout << "  " << "n = ";
+    cin >> n;
+    int* a = new int[n];
+    Print(a, n, 0);
+    cout << "  " << "S = " << Sum(a, n, 0) << endl;
+    delete[] a;
+    return 0;
 }
